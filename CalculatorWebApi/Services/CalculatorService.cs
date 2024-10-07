d66cd0b09417577770b3abb4fc1e70e2bcecbcad
soundhar <soundharnamakkal@gmail.com>
2024-10-07 18:40:48 -0400

    interfaces and services folder added

---
+++ CalculatorWebApi/Services/CalculatorService.cs
@@ -0,0 +1,31 @@
+using CalculatorWebApi.Interfaces;
+
+namespace CalculatorWebApi.Services
+{
+    public class CalculatorService : ICalculatorService<int>
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Division by zero is not allowed.");
+            }
+            return a / b;
+        }
+    }
+}
